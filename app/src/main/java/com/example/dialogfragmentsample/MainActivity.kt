f1c54b7c6e92dff511eb59a2bf14a296033a247a
shuhei_kiyosawa <s.kiyosawa@frontierfield.co.jp>
2020-12-04 17:32:38 +0900

    きもいやり方だけど二つのボタンでPickerで入力した値の受け入れ方を仕分ける

--- app/src/main/java/com/example/dialogfragmentsample/MainActivity.kt
+++ app/src/main/java/com/example/dialogfragmentsample/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.dialogfragmentsample
 
 import android.app.DatePickerDialog
-import android.app.DatePickerDialog.OnDateSetListener
-import android.app.Dialog
 import android.os.Bundle
 import android.view.View
 import android.widget.DatePicker
@@ -16,46 +14,31 @@ class MainActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
         setContentView(R.layout.activity_main)
     }
 
+    var clicked = 0
 
     // DatePickerDialogで入力された値を受け取る
     override fun onDateSet(view: DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int) {
         val str = String.format(Locale.US, "%d/%d/%d", year, monthOfYear+1, dayOfMonth)
-        textView1.text = str
+        if (clicked == 1) textView1.text = str
+        else textView2.text = str
 
     }
 
     // DatePickerダイアログを表示
     fun showDatePickerDialog(v: View) {
+        clicked = 1
         val newFragment = DatePickerDialog()
         newFragment.show(supportFragmentManager, "datePicker")
     }
 
-// これで二つの異なるダイアログ出せると思うんだけどよくわからない
-//    var from_year:Int = 0
-//    var from_month:Int = 0
-//    var from_day:Int = 0
-//    var to_year:Int = 0
-//    var to_month:Int = 0
-//    var to_day:Int = 0 //initialize them to current date in onStart()/onCreate()
-////    lateinit var from_dateListener:DatePickerDialog.OnDateSetListener
-////    lateinit var to_dateListener:DatePickerDialog.OnDateSetListener
-//
-//    var from_dateListener = object:OnDateSetListener() {
-//        override fun onDateSet(arg0:DatePicker, arg1:Int, arg2:Int, arg3:Int) {}
-//    }
-//    var to_dateListener = object:OnDateSetListener() {
-//        override fun onDateSet(arg0:DatePicker, arg1:Int, arg2:Int, arg3:Int) {}
-//    }
-//
-//    var DATE_PICKER_TO = 0
-//    var DATE_PICKER_FROM = 1
-//    override fun onCreateDialog(id:Int): Dialog {
-//        when (id) {
-//            DATE_PICKER_FROM -> return DatePickerDialog(this, from_dateListener, from_year, from_month, from_day)
-//            DATE_PICKER_TO -> return DatePickerDialog(this, to_dateListener, to_year, to_month, to_day)
-//        }
-//        return null
-//    }
+    // DatePickerダイアログを表示
+    fun showSecondDatePickerDialog(v: View) {
+        clicked = 2
+        val newFragment = DatePickerDialog()
+        newFragment.show(supportFragmentManager, "datePicker")
+    }
+
+
 
 }
 
